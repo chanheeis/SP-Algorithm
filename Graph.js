f4883b9d936b7929d29e8f32097c7116dfc4ad41
Chanhee <chanheeis@naver.com>
2019-10-10 16:58:22 +0900

    Execute DFS Algorithm with Graph

--- Graph.js
+++ Graph.js
@@ -1,40 +1,90 @@
+const util=require('util');
+
 class Graph {
     constructor(){
-        this.edges={};
+        this.edges=[];
         this.nodes=[];
     }
 
-    addNode(node){
-        if(!this.nodes.includes(node)){
-            this.nodes.push(node);
-            this.edges[node]=[];    
+    addNode(nodeName,marked=false){
+        for(let i=0;i<this.nodes.length;i++){
+            if(this.nodes[i].nodeName===nodeName) return;
         }
-        console.log(this.nodes, this.edges);
+        this.nodes.push({nodeName,marked});
+        this.edges[nodeName]=[];
     }
 
     addEdge(node1,node2){
-        this.edges[node1].push({node:node2});
-        this.edges[node2].push({node:node1});
-        console.log(this.edges);
+        const alreadyExist=this.edges[node1].includes(node2)
+        if(alreadyExist===true) return;
+        this.edges[node1].push(node2);
+        this.edges[node2].push(node1);
     }
 
     addDirectedEdge(node1,node2){
-        this.edges[node1].push({node:node2});
-        console.log(this.edges);
+        const alreadyExist=this.edges[node1].includes(node2);
+        if(alreadyExist===true) return;
+        this.edges[node1].push(node2);
+    }
+
+    DFS(startNode){
+        const root=this.getNode(startNode);
+        console.log(`Start Node : ${root.nodeName}`);
+
+        const stack=[];
+        stack.push(root);
+        root.marked=true;
+
+        while(stack.length!==0){
+            const node=stack.pop();
+            const edges=this.getEdges(node.nodeName);
+            edges.map(edge=>{
+                const edgeNode=this.getNode(edge);
+                if(edgeNode.marked===false){
+                    edgeNode.marked=true;
+                    stack.push(edgeNode);
+                }
+            })
+            console.log(`${node.nodeName} -> `);  
+        }
+    }
+
+    printNodes(){
+        console.log(`\n Nodes : \n ${util.inspect(this.nodes,{showHidden:false,depth:null})} \n`);
+    }
+
+    printEdges(){
+        console.log(`\n Edges : \n ${util.inspect(this.edges,{showHidden:false,depth:null})} \n`);
+    }
+ 
+    getNode(nodeName){
+        const node=this.nodes.find(node=>{
+            return node.nodeName===nodeName
+        })
+        return node;
+    }
+
+    getEdges(nodeName){
+        const edge=this.edges[nodeName];
+        return edge;
     }
 }
 
-//중복된 간선을 피하는 로직 설계해야 함
 const g=new Graph();
 
-g.addNode("node_1")
-g.addNode("node_1")
 g.addNode("node_1")
 g.addNode("node_2")
 g.addNode("node_3")
 g.addNode("node_4")
 g.addNode("node_5")
 
-g.addEdge("node_1","node_2");
-g.addEdge("node_1","node_4");
+g.addEdge("node_1","node_3");
+g.addEdge("node_3","node_5");
+g.addEdge("node_5","node_4");
+g.addEdge("node_2","node_4");
+
+g.printNodes();
+g.printEdges();
+
+g.DFS("node_1");
 
